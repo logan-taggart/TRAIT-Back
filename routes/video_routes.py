3fb29bf6108a213250fc8aed37876993fe799dec
Lane Keck <122398068+lanekooky@users.noreply.github.com>
2025-04-09 18:18:17 +0000

    updated routes to reflect front end

--- routes/video_routes.py
+++ routes/video_routes.py
@@ -6,18 +6,18 @@ CORS(video_blueprint)
 
 @video_blueprint.route("/detect-all", methods=["POST"])
 def detect_all():
-    if "video_file" not in request.files:
+    if "main_video" not in request.files:
         return {"error": "No file provided"}, 400
-    main_video = request.files["video_file"]
+    main_video = request.files["main_video"]
     bb_color = request.form.get("bb_color")
     return 
 
 
 @video_blueprint.route("/detect-specific", methods=["POST"])
 def detect_specific():
-    if "video_file" not in request.files or "reference_image" not in request.files:
+    if "main_video" not in request.files or "reference_image" not in request.files:
         return {"error": "No file provided"}, 400
-    main_video = request.files["video_file"]
+    main_video = request.files["main_video"]
     reference_image = request.files["reference_image"]
     similarity_threshold = int(request.form.get("confidence"))
     bb_color = request.form.get("bb_color")
